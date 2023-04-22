f9bfd7f26f9c7bb3f4619a4cf2b8f04ca96787be
Pedro <Pedro@DESKTOP-7MN609M>
2023-04-22 04:37:21 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hola Mundo");
+            System.Console.ReadKey();
+        }
+    }
+}
